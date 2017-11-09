e082749a5a8610a79976a4c72217f1ef5c840466
phil0s0pher <c.lambert1213@gmail.com>
2017-11-09 11:14:21 -0800

    Added tests for input domain and graph

--- tests/TestLeapYearGraph.java
+++ tests/TestLeapYearGraph.java
@@ -1,4 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestLeapYearGraph {
 
-
+    @Test
+    public void testIsLeap() {
+        assertTrue(LeapYear.isLeap(2004));
+        assertTrue(LeapYear.isLeap(2000));
+        assertFalse(LeapYear.isLeap(2300));
+        assertFalse(LeapYear.isLeap(2049));
+    }
 }
